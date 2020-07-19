9b5093cc4fad8c70d7479598d478150909657840
Norika23 <abcmnoyz23@gmail.com>
2020-07-20 01:42:50 +0900

    psg axios

---
+++ resources/js/custom.js
@@ -0,0 +1,14 @@
+let axis = require('axios');
+const { default: Axios } = require('axios');
+
+$('body').on('click', '.delete-bookmark', function() {
+    let id =  $(this).data('id');
+
+    axios.delete('/bookmarks/'+id)
+     .then(function(){
+        window.location.reload();
+     })
+     .catch(function(error){
+        console.log(error);
+     });
+});
